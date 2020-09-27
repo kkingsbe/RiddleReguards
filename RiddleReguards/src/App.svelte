760c86b4cba4e63166f90327021f89c9c4c03ebb
kkingsbe <kkingsbe@gmail.com>
2020-09-26 20:21:17 -0400

    First

---
+++ RiddleReguards/src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+	export let name;
+
+	import Posts from "./components/Posts.svelte"
+</script>
+
+<main>
+	<div class="title">
+		<h1>Posts:</h1>
+	</div>
+	<Posts></Posts>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		width: 100%;
+		height: 100%;
+	}
+
+	.title {
+		padding: 1em 0 1em 0;
+		width: 100%;
+		background: whitesmoke;
+		box-shadow: 3px 3px 5px 3px rgba(0,0,0,0.3);
+		position: fixed;
+	}
+
+	h1 {
+		font-size: 8vw;
+		font-weight: 100;
+		padding: 0px;
+		margin: 0px;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ RiddleReguards/src/components/Post.svelte
@@ -0,0 +1,42 @@
+<script>
+    export let id
+    export let content
+    import cssVars from 'svelte-css-vars'
+
+    var hue = id * 13
+    console.log(hue)
+    $: vars = {
+        hue: hue
+    }
+</script>
+
+<main use:cssVars={vars}>
+    <h1>#{id}</h1>
+    <p>{content}</p>
+</main>
+
+<style>
+    main {
+        background: hsl(var(--hue), 100%, 87%);
+        border-radius: 20px;
+        width: 80vw;
+        margin-bottom: 20px;
+        box-shadow: 3px 3px 5px 3px rgba(0,0,0,0.2);
+        padding: 10px;
+        font-size: 1.2em;
+    }
+
+    h1 {
+		font-size: 6vw;
+		font-weight: 100;
+		padding: 0px;
+        margin: 0px;
+        text-align: start;
+        padding: 5px 0 15px 5px;
+    }
+    
+    p {
+        padding: 0;
+        margin: 0;
+    }
+</style>
